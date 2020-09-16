361b602cbaf191cfda11c153661b9edf7108a48d
Romeo Mnxulwa <romeosihleGitHub.mnxulwa@gmail.com -f>
2020-09-16 16:57:20 -0700

    initial commit

---
+++ admin/include/db_config.php
@@ -0,0 +1,14 @@
+<?php
+    /*
+    define('DB_SERVER','localhost');
+    define('DB_USERNAME','root');
+    define('DB_PASSWORD','');
+    define('DB_DATABASE','hotel');
+    */
+
+    define('DB_SERVER','sql105.epizy.com');
+    define('DB_USERNAME','epiz_26747790');
+    define('DB_PASSWORD','16AHuWK4EBGvi5m');
+    define('DB_DATABASE','epiz_26747790_hotel');
+
+?>
